9d1cf78432febaf0323ca51ead944a7af60b05a0
Nikunj Saini <nikunjsaini08@gmail.com>
2025-07-08 20:42:00 +0530

    Add files via upload

---
+++ 9 type casting.py
@@ -0,0 +1,19 @@
+#variables and datatypes
+
+a=("hi")
+print("the type of a is",type(a))
+
+
+list1 = [3,4,5,6,7,6 ,("mango","apple")]
+print(type(list1))
+
+tuple1 = (("parrot","lion","nothing"))
+print(tuple1)
+
+
+
+# type casting
+a = "41"
+b=  "45"
+print(int(a)+int(b))
+
