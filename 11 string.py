832ac883b8d373243ab2b0d66717a49ce973e88e
Nikunj Saini <nikunjsaini08@gmail.com>
2025-07-09 18:46:44 +0530

    Add files via upload

---
+++ 11 string.py
@@ -0,0 +1,14 @@
+name = ("nikunj")
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+# short cut for above trick use fo loop
+hi = '''
+so many things i do'''
+for i in hi:
+    {
+    print(i)
+    }
