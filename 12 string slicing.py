d97c099a2a71c35b28a2f089999f68f44a52f270
Nikunj Saini <nikunjsaini08@gmail.com>
2025-07-10 23:33:00 +0530

    Add files via upload

---
+++ 12 string slicing.py
@@ -0,0 +1,14 @@
+# # string slicing
+hi = "nikunj"
+lenght = len(hi)
+print(lenght)
+print(hi[0:5])
+print(hi[:-5])
+#  # the hidden meaning of above line
+#  # print(apple[0:len(apple)-5])
+
+# kl ="harry"
+# print(len(kl))
+# print(kl[len(kl)-4:len(kl)-2])
+
+
