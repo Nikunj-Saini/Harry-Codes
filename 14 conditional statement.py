61f8cbd682b539efa8cbaad47fef6f6f1672514d
Nikunj Saini <nikunjsaini08@gmail.com>
2025-07-11 23:28:11 +0530

    Add files via upload

---
+++ 14 conditional statement.py
@@ -0,0 +1,15 @@
+x = float(input("enter your age: "))
+print("your age is:",x)
+   # conditional operator
+# print(x<18)
+# print(x>=18)
+# print(x==18)
+# print(x!=18)
+if (x>18):
+    print("yes you can drive vechile")
+    print("good to go")
+elif (x==18):
+    print("bhai ab toh tu meri gaadi leja")
+else:
+    print("sorry but you cannot drive vechile")
+    print("no")
