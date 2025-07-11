5685db9838c7fe95f0c7a445e382783336235314
Nikunj Saini <nikunjsaini08@gmail.com>
2025-07-11 23:28:21 +0530

    Add files via upload

---
+++ 15 exc2 .py
@@ -0,0 +1,17 @@
+import time
+a = 12
+b = 20
+timestamp = time.strftime('%H:%M:%S')
+print(timestamp)
+hour = int(time.strftime('%H'))
+minute = int(time.strftime('%M'))
+second = int(time.strftime('%S'))
+if hour < a:
+    print("GOOD MORNING")
+elif hour >= a and hour < b:
+    print("GOOD AFTERNOON")
+else:
+    print("GOOD NIGHT")
+
+
+
