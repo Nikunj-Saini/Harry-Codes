ad322768416bba9d72b7bd3fdb48914d199d0246
Nikunj Saini <nikunjsaini08@gmail.com>
2025-07-12 17:43:04 +0530

    Add files via upload

---
+++ 17 for loop.py
@@ -0,0 +1,10 @@
+# nm = "nikunj boy"
+# for i in nm:
+#     print(i)
+# colors = ["red","blue","green"]
+# for nm in colors:
+#     print(nm)
+#     for i in nm:
+#         print(i)
+for k in range(1,20001):
+    print(k)
