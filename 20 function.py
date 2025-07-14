74796f7dfa3657649c6bc9b4c45876d071e87f3e
Nikunj Saini <nikunjsaini08@gmail.com>
2025-07-14 21:24:53 +0530

    Add files via upload

---
+++ 20 function.py
@@ -0,0 +1,13 @@
+def ninja (a,b):
+    mean = (a*b)/(a+b)
+    print(mean)
+a = 50
+b = 40
+ninja(a,b)
+# for another value
+c = 47
+d = 29
+ninja(c,d)
+def yoyo(g,h):
+# when we want to write the baad me so we use pass function
+    pass
