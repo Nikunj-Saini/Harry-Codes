f884af80050d45f860256d89616f4dffc51fad55
Nikunj Saini <nikunjsaini08@gmail.com>
2025-07-15 22:29:49 +0530

    Add files via upload

---
+++ 24 tuppels.py
@@ -0,0 +1,24 @@
+# nik = (34,45,895,6,"nikunj")
+# print(type(nik),nik)
+# print(nik[0])
+# print(nik[1])
+# print(nik[2])
+# print(nik[3])
+# tup = nik[1:4]
+# print(tup)
+
+
+# tupples operations
+# countries = ("india","japan","england","focus","nikunj")
+# temp = list(countries)
+# temp.append("australia")#add item
+# temp.pop(3)# remove an item
+# temp[2] = "finland" #change item
+# countries = tuple(temp)
+# print(countries)
+
+
+tuple1 = (4,5,1,2,5,4,54,6,9,789)
+res = tuple1.count(5)
+res = tuple1.index(5,2,5)
+print(res)
