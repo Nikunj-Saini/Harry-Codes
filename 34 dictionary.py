e09c6e3809c16a3d796f1bd61abe52f86f234e0c
Nikunj Saini <nikunjsaini08@gmail.com>
2025-07-17 23:57:57 +0530

    Add files via upload

---
+++ 34 dictionary.py
@@ -0,0 +1,15 @@
+info = {'name':'Karan', 'age':19, 'eligible':True}
+# print(info)
+# print(info['name'])
+# print(info.get("name"))
+# print(info.keys())
+# print(info.values())
+
+# for k in info.keys():
+#   print(f"The value corresponding to the key {k} is {info[k]}")
+
+# pop()
+# del()
+# update()
+# popitem()
+# clear()
