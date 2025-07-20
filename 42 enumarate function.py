dedc9eadae6df54e38051fc12be698231e184d9a
Nikunj Saini <nikunjsaini08@gmail.com>
2025-07-20 12:33:48 +0530

    Enumarate Function completed

---
+++ 42 enumarate function.py
@@ -0,0 +1,13 @@
+marks = [12,43,5,54,19,85]
+# index =0
+# for k in marks:
+#     print (k)
+#     if(index ==2 ):
+#         print("yoyo nikunj")
+#     index +=1
+
+for index ,k in enumerate(marks):
+    print(k)
+    if (index == 2):
+        print("yoyo nikunj")
+n.
