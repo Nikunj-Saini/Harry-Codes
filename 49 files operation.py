c5ddad10104407dc1bddfe112a5e9de6337c33b4
Nikunj Saini <nikunjsaini08@gmail.com>
2025-07-21 12:19:06 +0530

    Learned How to Perform Operation on Files

---
+++ 49 files operation.py
@@ -0,0 +1,12 @@
+# READING A FILE
+
+# hi = open("myfile.txt",'r')
+# text = hi.read()
+# print(text)
+# hi.close()
+
+# WRITING A FILE
+
+hi = open("myfile.txt",'w')
+hi.write('hello world !')
+hi.close()
