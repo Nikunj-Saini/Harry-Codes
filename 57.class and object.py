790910c6ba74995881309860f4c2f263c69ec79b
Nikunj Saini <nikunjsaini08@gmail.com>
2025-07-21 12:19:53 +0530

    Learned About the concept of Class and Object

---
+++ 57.class and object.py
@@ -0,0 +1,18 @@
+class pundu:
+    name = "nikunj"
+    occupation = "softwares debugger"
+    salarypackaage = 99
+
+    def info(self):
+         print(f"{self.name}  is a {self.occupation}")
+
+a = pundu()
+a.name = "shubhi"
+a.occupation = "secretotry"
+a.info()
+
+
+b = pundu()
+b.name = "kashish"
+b.occupation = " personal secretotry "
+b.info()
